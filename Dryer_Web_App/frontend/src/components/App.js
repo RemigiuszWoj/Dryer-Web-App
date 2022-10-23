29fa127767910b4c0ca5368412ac350ad0886100
RemigiuszWoj <remigiusz.wojewodzki@gmail.com>
2022-10-23 20:31:29 +0200

    rebuild frontend

--- Dryer_Web_App/frontend/src/components/App.js
+++ Dryer_Web_App/frontend/src/components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-            <h1> siema11111</h1>
+            <h1> siema1dddd1111</h1>
       </div>
     );
   }
